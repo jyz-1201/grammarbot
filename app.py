20acb52bcccb08531c19efd8a1c8dcba26c3cd74
jyz-1201 <663283835@qq.com>
2022-04-23 09:21:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,18 @@
-from datetime import datetime
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask import Flask, request
+from flask_restful import Resource, Api
+
 app = Flask(__name__)
+api = Api(app)
 
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': request.form['username']}
+    def put(self):
+        return {"put_hello": request.form['username']}
+    def post(self):
+        return {"posted_hello": "res"}
 
-@app.route('/')
-def index():
-   print('Request for index page received')
-   return render_template('index.html')
+api.add_resource(HelloWorld, '/login')
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
-
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
-
-
-if __name__ == '__main__':
-   app.run()
+if __name__ == "__main__":
+    app.run()
